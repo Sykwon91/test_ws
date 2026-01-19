8eb5f6c004a3469d5e712566322d2d11caeb94c2
Sangyun Kwon <ksu619@naver.com>
2026-01-20 02:16:50 +0900

    upload icp

---
+++ icp/build/CMakeFiles/pointcloud_example.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pointcloud_example.dir/src/icp.cpp.o"
+  "CMakeFiles/pointcloud_example.dir/src/icp.cpp.o.d"
+  "pointcloud_example"
+  "pointcloud_example.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pointcloud_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
